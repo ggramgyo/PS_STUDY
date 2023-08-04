eb9acd23242d67fd8e6194aae323c3f7d63297dd
ggramgyo <88009952+ggramgyo@users.noreply.github.com>
2023-08-04 16:07:53 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 116 ms, Memory: 19,844 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,47 +1,57 @@
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
-import java.io.FileInputStream;
-import java.util.ArrayDeque;
-
-class Solution
-{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+public class Solution {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         for (int testcase = 1; testcase <= 10; testcase++) {
-            int n = sc.nextInt();
+            int n = Integer.parseInt(br.readLine());
             int[] arr = new int[8];
-            int[] result = new int[8];
+            String[] c = br.readLine().split(" ");
             for (int i = 0; i < 8; i++) {
-                arr[i] = sc.nextInt();
+                arr[i] = Integer.parseInt(c[i]);
             }
             int count = 0;
             int index = 0;
             int value = 1;
             while (true) {
-                if (arr[index] - value <= 0) {
-                    arr[index] = 0;
-                    break;
-                } else {
-                    arr[index] -= value;
-                }
+            	if(arr[index] - value <= 0) {
+            		arr[index] = 0;
+            		break;
+            	}else {
+            		arr[index] -= value;
+            	}
                 index = (index + 1) % 8;
                 value = (value + 1) % 6;
                 if (value == 0) {
-                    value++;
+                	value ++;
                 }
-                count++;
+                count ++;
             }
             int remain = count % 8;
-
-            System.out.print("#" + testcase + " ");
+            
+            bw.write("#");
+            bw.write(String.valueOf(testcase));
+            bw.write(" ");
             for (int i = (remain + 1) % 8; i < 8; i++) {
-                System.out.print(arr[i] + " ");
-            }
+            	bw.write(String.valueOf((arr[i])));
+                bw.write(" ");
+			}
             for (int i = 0; i <= remain % 8; i++) {
-                System.out.print(arr[i] + " ");
-            }
+            	bw.write(String.valueOf((arr[i])));
+                bw.write(" ");
+			}
 
-            System.out.println();
+            bw.write("\n");
         }
+        bw.flush();
+        bw.close();
     }
 }

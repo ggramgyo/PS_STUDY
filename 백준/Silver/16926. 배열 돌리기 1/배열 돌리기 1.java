18c4578ad3cb2dbec4391275886a0c9e96305517
ggramgyo <88009952+ggramgyo@users.noreply.github.com>
2023-08-08 15:41:56 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 1824 ms, Memory: 133456 KB -BaekjoonHub

---
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        int[][] graph = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int r = 0; r < R; r++) {
+            boolean[][] chk = new boolean[N][M];
+            for (int start = 0; start < Math.min(N, M); start++) {
+                if (!chk[start][start]) {
+                    chk[start][start] = true;
+                    rotate(start, start, 0, graph[start][start], graph, chk);
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void rotate(int x, int y, int direction, int temp, int[][] graph, boolean[][] chk) {
+        while (direction < 4) {
+            x += dx[direction];
+            y += dy[direction];
+            if (0 <= x && x < graph.length && 0 <= y && y < graph[0].length) {
+                if (!chk[x][y]) {
+                    int next_temp = graph[x][y];
+                    graph[x][y] = temp;
+                    temp = next_temp;
+                    chk[x][y] = true;
+                } else {
+                    x -= dx[direction];
+                    y -= dy[direction];
+                    direction++;
+                }
+            } else {
+                x -= dx[direction];
+                y -= dy[direction];
+                direction++;
+            }
+        }
+        graph[x + dx[direction - 1]][y + dy[direction - 1]] = temp;
+    }
+}

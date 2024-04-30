4044df1bc4cc715f8ab5a271ff2cd405ad4429fa
ggramgyo <88009952+ggramgyo@users.noreply.github.com>
2024-04-30 16:34:36 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 112 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,26 +1,29 @@
 import sys
 from heapq import heappush, heappop
-rx = [0, 0, 1, -1]
-ry = [1, -1, 0, 0]
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
 idx = 1
 while True:
     cmd = int(sys.stdin.readline())
-    if not cmd: break
+    if not cmd:
+        break
+
     chk = [[False] * cmd for _ in range(cmd)]
     graph = [list(map(int, sys.stdin.readline().split())) for _ in range(cmd)]
     heap = [(graph[0][0], 0, 0)]
+
     while heap:
         v, x, y = heappop(heap)
-        if x == cmd-1 and y == cmd-1:
+        if x == y == cmd - 1:
             print("Problem {}: {}".format(idx, v))
             break
 
-        for _ in range(4):
-            nx = rx[_]+ x
-            ny = ry[_]+ y
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
             if 0 <= nx < cmd and 0 <= ny < cmd and not chk[nx][ny]:
                 chk[nx][ny] = True
-                heappush(heap, (graph[nx][ny] + v, nx, ny))
-
+                heappush(heap, (v + graph[nx][ny], nx, ny))
 
     idx += 1
